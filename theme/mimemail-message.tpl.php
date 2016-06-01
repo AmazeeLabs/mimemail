149b6c5e684cbd148f4386330de75fbab3862336
Vasile Chindris <vasile.chindris@amazee.com>
2016-06-01 09:31:48 +0200

    FOR-2044 Temporary template change.

--- theme/mimemail-message.tpl.php
+++ theme/mimemail-message.tpl.php
@@ -1,40 +1,103 @@
-<?php
-
-/**
- * @file
- * Default theme implementation to format an HTML mail.
- *
- * Copy this file in your default theme folder to create a custom themed mail.
- * Rename it to mimemail-message--[module]--[key].tpl.php to override it for a
- * specific mail.
- *
- * Available variables:
- * - $recipient: The recipient of the message
- * - $subject: The message subject
- * - $body: The message body
- * - $css: Internal style sheets
- * - $module: The sending module
- * - $key: The message identifier
- *
- * @see template_preprocess_mimemail_message()
- */
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <?php if ($css): ?>
-    <style type="text/css">
-      <!--
-      <?php print $css ?>
-      -->
-    </style>
-    <?php endif; ?>
-  </head>
-  <body id="mimemail-body" <?php if ($module && $key): print 'class="'. $module .'-'. $key .'"'; endif; ?>>
-    <div id="center">
-      <div id="main">
-        <?php print $body ?>
-      </div>
-    </div>
-  </body>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="initial-scale=1.0">
+  <meta name="format-detection" content="telephone=no">
+  <title>Email Template</title>
+  <style type="text/css" data-premailer="ignore">
+    .ReadMsgBody { width: 100%; background-color: #ebebeb;}
+    .ExternalClass {width: 100%; background-color: #ebebeb;}
+    .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {line-height:100%;}
+    body {-webkit-text-size-adjust:none; -ms-text-size-adjust:none;}
+    body {margin:0; padding:0;}
+    table {border-spacing:0;}
+    table td {border-collapse:collapse;}
+    .yshortcuts a {border-bottom: none !important;}
+    @media screen and (max-width: 600px), screen and (max-device-width: 600px) {
+      table[class="container"] { width: 95% !important; }
+    }
+    @media screen and (max-width: 480px), screen and (max-device-width: 480px) {
+      td[class="container-padding"] { padding-left: 12px !important; padding-right: 12px !important; }
+    }
+  </style>
+  <style type="text/css">
+    body { background-color: #f0f0f0; padding: 10px 0; margin: 10px 0; }
+    table.container { box-shadow: 0 0 3px #ddd; border-bottom: 1px solid #e0e0e0; font-family: Helvetica, Verdana, sans-serif; color: #434343; }
+    table.container td { border-left: 1px solid #e0e0e0; }
+    table.container td.header div { height: 5px; /*border: 1px solid #042f6d;*/ background-color: #3e71b8; /*-webkit-border-radius: 2px; -moz-border-radius: 2px; border-radius: 2px; box-shadow: inset 0 1px 0 #3e6aaa;*/ }
+    table.container td.logo { text-align: center; padding: 20px 0; border-bottom: 1px solid #efefef; }
+    h1, h2, h3, h4, h5, h6 { color: #3e71b8; line-height: 1.5; margin: 0 0 25px 0; }
+    h1 { font-size: 22px; } h2 { font-size: 18px; } h3 { font-size: 16px; } h4, h5, h6 { font-size: 14px; }
+    p { font-size: 14px; line-height: 1.5; margin: 0 0 25px 0; color: 4877BC; }
+    a { color: #3e71b8; text-decoration: underline; }
+    td.container-padding img { border: 0px solid #ddd; padding: 0px; }
+    a.intercom-h2b-button { background-color: #3e71b8; border-radius: 5px; /*border: 1px solid rgba(0,0,0,0.2);*/ color:#fff; display: inline-block; font-size: 15px; font-weight: bold; min-height: 20px; padding: 9px 15px; text-decoration: none; }
+    ul li, ol li { font-size: 14px; line-height: 1.5; }
+    table.footer { border-top: 1px solid #efefef; width: 100%; height: 69px; }
+    table.footer td { vertical-align: middle; text-align: center; color: #434343; font-size: 12px; }
+    table.footer td.social { width: 60%; line-height: 20px; border-left: none; }
+    table.footer td.social a { font-weight: bold; text-decoration: none; }
+    table.footer td.social a.twitter { color: #00aced; }
+    table.footer td.social a.facebook { color: #6d83bc; }
+    table.footer td.social img { vertical-align: middle; }
+    table.footer td.subscription { width: 40%; }
+    table.footer td.subscription a { color: #434343; }
+
+    .avatar_container-padding { padding: 0 30px 20px 30px; }
+    .avatar { float: left; display: block; width: 40px; height: 40px; -webkit-border-radius: 20px; border-radius: 20px; -webkit-background-clip: padding-box; background-clip: padding-box; }
+    .admin_name { float: left; margin: 11px 0 0 10px; }
+  </style>
+</head>
+<body style="margin:0; padding:10px 0;" bgcolor="#f0f0f0">
+<br>
+<table border="0" width="100%" height="100%" cellpadding="0" cellspacing="0" bgcolor="#f0f0f0">
+  <tr>
+    <td align="center" valign="top" bgcolor="#f0f0f0" style="background-color: #f0f0f0;">
+      <table width="600" cellpadding="0" cellspacing="0" border="0" class="container" bgcolor="#ffffff">
+        <tr>
+          <td class="header">
+            <div>�</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="logo">
+            <img src="https://www.for.org/sites/default/files/logo200px.png" title="FOR Logo">
+          </td>
+        </tr>
+        <tr>
+          <td class="container-padding" bgcolor="#ffffff" style="background-color: #ffffff; padding-left: 30px; padding-right: 30px;">
+            <br>
+            <?php print $body ?>
+          </td>
+        </tr>
+        <br>
+        <br>
+        <tr>
+          <td class="avatar_container-padding">
+            <div class="avatar_wrapper" width="50" height="40" align="left">
+              <img src="https://static.intercomassets.com/avatars/137801/square_50/Joel_Samsinger.jpg?1439199360" height="40" width="40" class="avatar" alt=" ">
+            </div>
+            <div class="admin_name">
+              <b>Joel Samsinger</b> from FOR
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <table border="0" width="100%" height="100%" cellpadding="0" cellspacing="0" bgcolor="#fbfbfb" class="footer">
+              <tr>
+                <td class="social">
+                  Follow us:<a href="https://twitter.com/for_org" class="twitter"><img src="http://cl.ly/image/3F1s2K34012V/twitter.png" width="20" height="20"> Twitter</a> <a href="https://www.facebook.com/for.organisation" class="facebook"><img src="http://cl.ly/image/3P3M3S1T093M/facebook.png" width="20" height="20">  Facebook</a>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+<br>
+</body>
 </html>
